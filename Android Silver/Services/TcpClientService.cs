a8b995506de0c04154d138354613536dd43686f1
DmitryMaltsev <71661891+DmitryMaltsev@users.noreply.github.com>
2026-09-14 09:48:26 +0300

    123

--- Android Silver/Services/TcpClientService.cs
+++ Android Silver/Services/TcpClientService.cs
@@ -3,7 +3,6 @@ using Android_Silver.Entities.FBEntities;
 using Android_Silver.Entities.Modes;
 using Android_Silver.Entities.Visual;
 using Android_Silver.Entities.Visual.Menus;
-
 using System.Net.Sockets;
 using System.Text;
 
